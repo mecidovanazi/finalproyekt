ca30d6fb9556cf979016ccc03f1aab79873424f0
mecidovanazi <mjdvanazi77@gmail.com>
2022-04-20 11:30:57 +0400

    proyektin ilk merhelesi

---
+++ main.js
@@ -0,0 +1,5 @@
+const name ="Nezaket"
+// const name ="Xedice"
+
+let sureName = "Mecidova"
+// let sureName = "Qanzayeva"
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+
+
+
+function App() {
+  return (
+   
+   <div className='containir-fluid'>
+   <Navbar/>
+   
+   </div>
+  
+  );
+}
+
+export default App;
