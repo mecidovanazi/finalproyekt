5a92af4a2e43c032c1ecd506011dfaa36fbb7142
mecidovanazi <mjdvanazi77@gmail.com>
2022-04-21 15:46:55 +0400

    Experience-nin yaradilmasi

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Services from './components/Services';
+import Experience from './components/Experience';
 
 
 
@@ -20,6 +21,7 @@ function App () {
 <Header/>
 <AboutMe/>
 <Services/>
+<Experience/>
 
   </div>
   
---
+++ src/components/Experience.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Experience = () => {
+    return (
+      <div className='experience'  >
+        <div className="d-flex justify-content-center my-5">
+          <h1>Experience</h1>
+        </div>
+        <div className="container experience-wrapper">
+  
+          <div className="timeline-block timeline-block-right">
+            <div className="marker"></div>
+            <div className="timeline-content">
+              <h3>2012-2013</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae inventore sapiente reiciendis esse dolorem culpa dolorum temporibus possimus deserunt perferendis.</p>
+            </div>
+          </div>
+  
+          <div className="timeline-block timeline-block-left">
+            <div className="marker"></div>
+            <div className="timeline-content">
+              <h3>2012-2013</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae inventore sapiente reiciendis esse dolorem culpa dolorum temporibus possimus deserunt perferendis.</p>
+            </div>
+          </div>
+  
+          <div className="timeline-block timeline-block-right">
+            <div className="marker"></div>
+            <div className="timeline-content">
+              <h3>2012-2013</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae inventore sapiente reiciendis esse dolorem culpa dolorum temporibus possimus deserunt perferendis.</p>
+            </div>
+          </div>
+  
+          <div className="timeline-block timeline-block-left">
+            <div className="marker"></div>
+            <div className="timeline-content">
+              <h3>2012-2013</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae inventore sapiente reiciendis esse dolorem culpa dolorum temporibus possimus deserunt perferendis.</p>
+            </div>
+          </div>
+  
+        </div>
+  
+      </div>
+    )
+  }
+
+export default Experience
